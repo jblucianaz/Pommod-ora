397ccfdd2825c34ac90969d379ebed52df19be3f
Jean-Baptiste Lucianaz <jb.lucianaz@gmail.com>
2020-08-04 11:12:26 +0200

    Initial Commit

---
+++ Pommod'ora/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pommod'ora
+//
+//  Created by Jb Lucianaz on 04/08/2020.
+//  Copyright © 2020 Jb Lucianaz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
